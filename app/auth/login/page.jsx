099c7aabeb0892c0ae89188a79d56ee3a7364bde
Raynald Escala <raynaldescala01@gmail.com>
2025-03-01 22:41:37 +0800

    update sign-up link path in login page

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -144,7 +144,7 @@ export default function LoginPage() {
                         </Form>
                         <div className="mt-4 text-center text-sm">
                             Don&apos;t have an account?{" "}
-                            <Link href="#" className="underline">
+                            <Link href="/auth/sign-up" className="underline">
                                 Sign up
                             </Link>
                         </div>
