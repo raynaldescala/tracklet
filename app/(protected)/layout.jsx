a87b6e4eb00dbf73381ed57795394fd9b01f435b
Raynald Escala <raynaldescala01@gmail.com>
2025-03-06 23:44:24 +0800

    update dashboard header

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,7 +1,13 @@
 import { AppSidebar } from "@/app/components/ui/app-sidebar";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/app/components/ui/avatar";
 import { Button } from "@/app/components/ui/button";
 import { SidebarProvider, SidebarTrigger } from "@/app/components/ui/sidebar";
-import { logout } from "@/lib/supabase/auth/actions";
+import ThemeSwitcher from "@/app/components/ui/theme-switcher";
+import { CircleFadingPlus } from "lucide-react";
 
 export default function Layout({ children }) {
     return (
@@ -9,10 +15,19 @@ export default function Layout({ children }) {
             <AppSidebar />
             <div className="flex w-full flex-col">
                 <div className="sticky top-0 z-50 flex h-16 w-full items-center border-b bg-background shadow-sm">
-                    <div className="w-full px-4">
+                    <div className="w-full px-[10px] sm:px-[18px]">
                         <header className="flex items-center justify-between py-2">
                             <SidebarTrigger />
-                            <Button onClick={logout}>Logout</Button>
+                            <div className="flex items-center gap-4">
+                                <Button variant="ghost" size="icon">
+                                    <CircleFadingPlus className="animate-pulse text-primary" />
+                                </Button>
+                                <ThemeSwitcher />
+                                <Avatar className="h-9 w-9">
+                                    <AvatarImage src="" alt="" />
+                                    <AvatarFallback>RE</AvatarFallback>
+                                </Avatar>
+                            </div>
                         </header>
                     </div>
                 </div>
