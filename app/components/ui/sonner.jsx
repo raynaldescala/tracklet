5261d569233d47ab82203b054c61f5869cbce73b
Raynald Escala <raynaldescala01@gmail.com>
2025-03-15 22:48:55 +0800

    update sonner component

--- app/components/ui/sonner.jsx
+++ app/components/ui/sonner.jsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }) => {
             className="toaster group"
             toastOptions={{
                 classNames: {
-                    toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+                    toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg font-sans",
                     description: "group-[.toast]:text-muted-foreground",
                     actionButton:
                         "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
