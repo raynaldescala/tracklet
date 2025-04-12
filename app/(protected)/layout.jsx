0466e2c06d43dd09cd80bbf4c8992c7f33f466e6
Raynald Escala <raynaldescala01@gmail.com>
2025-04-12 20:40:39 +0800

    integrate application form and context provider

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AppSidebar } from "@/app/components/ui/app-sidebar";
+import ApplicationForm from "@/app/components/ui/application-form";
 import {
     Avatar,
     AvatarFallback,
@@ -21,9 +22,10 @@ import { Skeleton } from "@/app/components/ui/skeleton";
 import ThemeSwitcher from "@/app/components/ui/theme-switcher";
 import { fetchProfile } from "@/lib/supabase/applications/actions";
 import { logout } from "@/lib/supabase/auth/actions";
-import { createClient } from "@/lib/supabase/client";
 import { CircleFadingPlus, LogOut, Settings, User } from "lucide-react";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { ApplicationsProvider } from "../contexts/applications-context";
 
 export default function Layout({ children }) {
     const [profile, setProfile] = useState(null);
@@ -50,47 +52,31 @@ export default function Layout({ children }) {
     };
 
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <div className="flex w-full flex-col">
-                <div className="sticky top-0 z-50 flex h-16 w-full items-center border-b bg-background shadow-sm">
-                    <div className="w-full px-[10px] sm:px-[18px]">
-                        <header className="flex items-center justify-between py-2">
-                            <SidebarTrigger />
-                            <div className="flex items-center gap-4">
-                                <Button variant="ghost" size="icon">
-                                    <CircleFadingPlus className="animate-pulse text-primary" />
-                                </Button>
-                                <ThemeSwitcher />
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button
-                                            variant="ghost"
-                                            className="relative h-9 w-9 rounded-full"
-                                        >
-                                            <Avatar className="h-9 w-9">
-                                                {profile?.avatar_url ? (
-                                                    <AvatarImage
-                                                        src={profile.avatar_url}
-                                                        alt={`${profile?.name || "User"}'s avatar`}
-                                                    />
-                                                ) : (
-                                                    <AvatarFallback>
-                                                        {getFirstLetter()}
-                                                    </AvatarFallback>
-                                                )}
-                                            </Avatar>
+        <ApplicationsProvider>
+            <SidebarProvider>
+                <AppSidebar />
+                <div className="flex w-full flex-col">
+                    <div className="sticky top-0 z-50 flex h-16 w-full items-center border-b bg-background shadow-sm">
+                        <div className="w-full px-[10px] sm:px-[18px]">
+                            <header className="flex items-center justify-between py-2">
+                                <SidebarTrigger />
+                                <div className="flex items-center gap-4">
+                                    <ApplicationForm>
+                                        <Button variant="ghost" size="icon">
+                                            <CircleFadingPlus className="animate-pulse text-primary" />
                                         </Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent align="end">
-                                        <DropdownMenuLabel className="pr-4 font-normal">
-                                            <div className="flex items-center gap-3">
-                                                <Avatar className="h-8 w-8">
+                                    </ApplicationForm>
+                                    <ThemeSwitcher />
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                variant="ghost"
+                                                className="relative h-9 w-9 rounded-full"
+                                            >
+                                                <Avatar className="h-9 w-9">
                                                     {profile?.avatar_url ? (
                                                         <AvatarImage
-                                                            src={
-                                                                profile.avatar_url
-                                                            }
+                                                            src={profile.avatar_url}
                                                             alt={`${profile?.name || "User"}'s avatar`}
                                                         />
                                                     ) : (
@@ -99,48 +85,68 @@ export default function Layout({ children }) {
                                                         </AvatarFallback>
                                                     )}
                                                 </Avatar>
-                                                <div className="grid space-y-1">
-                                                    {profile?.name ? (
-                                                        <p className="font-medium leading-none">
-                                                            {profile.name}
-                                                        </p>
-                                                    ) : (
-                                                        <Skeleton className="h-4 w-24" />
-                                                    )}
-                                                    {profile?.email ? (
-                                                        <p className="text-xs leading-none text-muted-foreground">
-                                                            {profile.email}
-                                                        </p>
-                                                    ) : (
-                                                        <Skeleton className="h-3 w-32" />
-                                                    )}
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel className="pr-4 font-normal">
+                                                <div className="flex items-center gap-3">
+                                                    <Avatar className="h-8 w-8">
+                                                        {profile?.avatar_url ? (
+                                                            <AvatarImage
+                                                                src={
+                                                                    profile.avatar_url
+                                                                }
+                                                                alt={`${profile?.name || "User"}'s avatar`}
+                                                            />
+                                                        ) : (
+                                                            <AvatarFallback>
+                                                                {getFirstLetter()}
+                                                            </AvatarFallback>
+                                                        )}
+                                                    </Avatar>
+                                                    <div className="grid space-y-1">
+                                                        {profile?.name ? (
+                                                            <p className="font-medium leading-none">
+                                                                {profile.name}
+                                                            </p>
+                                                        ) : (
+                                                            <Skeleton className="h-4 w-24" />
+                                                        )}
+                                                        {profile?.email ? (
+                                                            <p className="text-xs leading-none text-muted-foreground">
+                                                                {profile.email}
+                                                            </p>
+                                                        ) : (
+                                                            <Skeleton className="h-3 w-32" />
+                                                        )}
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </DropdownMenuLabel>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuGroup>
-                                            <DropdownMenuItem>
-                                                <User className="mr-1 h-4 w-4" />
-                                                <span>Account</span>
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem>
-                                                <Settings className="mr-1 h-4 w-4" />
-                                                <span>Settings</span>
-                                            </DropdownMenuItem>
+                                            </DropdownMenuLabel>
                                             <DropdownMenuSeparator />
-                                            <DropdownMenuItem onClick={logout}>
-                                                <LogOut className="mr-1 h-4 w-4" />
-                                                <span>Log out</span>
-                                            </DropdownMenuItem>
-                                        </DropdownMenuGroup>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </div>
-                        </header>
+                                            <DropdownMenuGroup>
+                                                <DropdownMenuItem>
+                                                    <User className="mr-1 h-4 w-4" />
+                                                    <span>Account</span>
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    <Settings className="mr-1 h-4 w-4" />
+                                                    <span>Settings</span>
+                                                </DropdownMenuItem>
+                                                <DropdownMenuSeparator />
+                                                <DropdownMenuItem onClick={logout}>
+                                                    <LogOut className="mr-1 h-4 w-4" />
+                                                    <span>Log out</span>
+                                                </DropdownMenuItem>
+                                            </DropdownMenuGroup>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+                            </header>
+                        </div>
                     </div>
+                    <main className="flex-1 p-4 sm:p-6">{children}</main>
                 </div>
-                <main className="flex-1 p-4 sm:p-6">{children}</main>
-            </div>
-        </SidebarProvider>
+            </SidebarProvider>
+        </ApplicationsProvider>
     );
 }
