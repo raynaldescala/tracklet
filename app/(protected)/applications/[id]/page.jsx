56ba178569f0a6ebaa1abc779ef02f8d2b6747ee
Raynald Escala <raynaldescala01@gmail.com>
2025-05-04 23:41:40 +0800

    add loading state with skeletons and disable actions

--- app/(protected)/applications/[id]/page.jsx
+++ app/(protected)/applications/[id]/page.jsx
@@ -10,14 +10,19 @@ import {
     CardTitle,
 } from "@/app/components/ui/card";
 import { Separator } from "@/app/components/ui/separator";
+import { Skeleton } from "@/app/components/ui/skeleton";
 import { Textarea } from "@/app/components/ui/textarea";
-import { fetchApplicationById, updateApplicationNotes } from "@/lib/supabase/applications/actions";
+import {
+    fetchApplicationById,
+    updateApplicationNotes,
+} from "@/lib/supabase/applications/actions";
 import { format } from "date-fns";
 import {
     ArrowLeft,
     Calendar,
     Edit,
     ExternalLink,
+    Loader2,
     MapPin,
     Tag,
     Trash,
@@ -28,6 +33,7 @@ import { toast } from "sonner";
 
 export default function ApplicationDetailsPage({ params }) {
     const [application, setApplication] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const [notes, setNotes] = useState("");
     const { id } = use(params);
 
@@ -35,10 +41,13 @@ export default function ApplicationDetailsPage({ params }) {
         async function getApplication() {
             try {
                 const data = await fetchApplicationById(id);
+                console.log(data);
                 setApplication(data);
-                setNotes(data?.notes || "")
+                setNotes(data?.notes || "");
             } catch (error) {
                 console.error("Error fetching application:", error);
+            } finally {
+                setIsLoading(false);
             }
         }
 
@@ -46,17 +55,20 @@ export default function ApplicationDetailsPage({ params }) {
     }, [id]);
 
     async function handleSaveNotes() {
+        setIsLoading(true);
+
         try {
             const result = await updateApplicationNotes(id, notes);
             if (result.error) {
                 toast.error("Failed to update notes: " + result.error);
             } else {
                 toast.success("Notes updated successfully");
-                setApplication(prev => ({ ...prev, notes }));
+                setApplication((prev) => ({ ...prev, notes }));
             }
-        }
-        catch (error) {
-            toast.error("An unexpected error occured")
+        } catch (error) {
+            toast.error("An unexpected error occured");
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -85,25 +97,35 @@ export default function ApplicationDetailsPage({ params }) {
                     <ArrowLeft className="h-4 w-4" />
                     Back
                 </Link>
-                <div className="flex flex-col justify-between gap-4 sm:flex-row">
-                    <div>
-                        <h2 className="text-3xl font-bold tracking-tight">
-                            {application?.position}
-                        </h2>
-                        <p className="text-muted-foreground">
-                            {application?.company}
-                        </p>
+                <div className="mt-1 flex flex-col justify-between gap-4 sm:flex-row">
+                    <div className="grid gap-2">
+                        {application?.position ? (
+                            <h2 className="text-3xl font-bold leading-none tracking-tight">
+                                {application.position}
+                            </h2>
+                        ) : (
+                            <Skeleton className="h-9 w-72" />
+                        )}
+                        {application?.company ? (
+                            <p className="leading-none text-muted-foreground">
+                                {application?.company}
+                            </p>
+                        ) : (
+                            <Skeleton className="h-4 w-28" />
+                        )}
                     </div>
                     <div className="flex items-center gap-2">
                         <Button
                             variant="outline"
                             className="h-9 w-9 sm:h-10 sm:w-10"
+                            disabled={isLoading}
                         >
                             <Edit className="h-4 w-4" />
                         </Button>
                         <Button
                             variant="destructive"
                             className="h-9 w-9 sm:h-10 sm:w-10"
+                            disabled={isLoading}
                         >
                             <Trash className="h-4 w-4" />
                         </Button>
@@ -126,13 +148,17 @@ export default function ApplicationDetailsPage({ params }) {
                                 <p className="mb-1 text-sm font-medium text-muted-foreground">
                                     Status
                                 </p>
-                                <Badge
-                                    className={`font-semibold ${getStatusBadge("Applied")}`}
-                                >
-                                    <span className="text-slate-100">
-                                        {application?.status}
-                                    </span>
-                                </Badge>
+                                {application?.status ? (
+                                    <Badge
+                                        className={`font-semibold ${getStatusBadge("Applied")}`}
+                                    >
+                                        <span className="text-slate-100">
+                                            {application.status}
+                                        </span>
+                                    </Badge>
+                                ) : (
+                                    <Skeleton className="h-[21.6px] w-16" />
+                                )}
                             </div>
                             <div>
                                 <p className="mb-1 text-sm font-medium text-muted-foreground">
@@ -140,13 +166,16 @@ export default function ApplicationDetailsPage({ params }) {
                                 </p>
                                 <div className="flex items-center">
                                     <Calendar className="mr-2 h-4 w-4 text-muted-foreground" />
-                                    <p>
-                                        {application?.date_applied &&
-                                            format(
+                                    {application?.date_applied ? (
+                                        <p>
+                                            {format(
                                                 application.date_applied,
                                                 "MMM d, yyyy",
                                             )}
-                                    </p>
+                                        </p>
+                                    ) : (
+                                        <Skeleton className="h-6 w-[90px]" />
+                                    )}
                                 </div>
                             </div>
                         </div>
@@ -158,10 +187,14 @@ export default function ApplicationDetailsPage({ params }) {
                                 </p>
                                 <div className="flex items-center">
                                     <MapPin className="mr-2 h-4 w-4 text-muted-foreground" />
-                                    <p>
-                                        {application?.location ||
-                                            "No location provided"}
-                                    </p>
+                                    {application ? (
+                                        <p>
+                                            {application?.location ||
+                                                "No location provided"}
+                                        </p>
+                                    ) : (
+                                        <Skeleton className="h-6 w-20"></Skeleton>
+                                    )}
                                 </div>
                             </div>
                             <div>
@@ -170,10 +203,14 @@ export default function ApplicationDetailsPage({ params }) {
                                 </p>
                                 <div className="flex items-center">
                                     <Tag className="mr-2 h-4 w-4 text-muted-foreground" />
-                                    <p>
-                                        {application?.source ||
-                                            "No source provided"}
-                                    </p>
+                                    {application ? (
+                                        <p>
+                                            {application?.source ||
+                                                "No source provided"}
+                                        </p>
+                                    ) : (
+                                        <Skeleton className="h-6 w-20"></Skeleton>
+                                    )}
                                 </div>
                             </div>
                             <div>
@@ -182,23 +219,29 @@ export default function ApplicationDetailsPage({ params }) {
                                 </p>
                                 <div className="flex items-center">
                                     <ExternalLink className="mr-2 h-4 w-4 text-muted-foreground" />
-                                    {application?.application_link ? (
-                                        <Button
-                                            asChild
-                                            variant="link"
-                                            className="h-fit p-0 text-base"
-                                        >
-                                            <Link
-                                                target="_blank"
-                                                href={
-                                                    application.application_link
-                                                }
+                                    {application ? (
+                                        application?.application_link ? (
+                                            <Button
+                                                asChild
+                                                variant="link"
+                                                className="h-fit p-0 text-base"
                                             >
-                                                {application.application_link}
-                                            </Link>
-                                        </Button>
+                                                <Link
+                                                    target="_blank"
+                                                    href={
+                                                        application.application_link
+                                                    }
+                                                >
+                                                    {
+                                                        application.application_link
+                                                    }
+                                                </Link>
+                                            </Button>
+                                        ) : (
+                                            <p>No link provided</p>
+                                        )
                                     ) : (
-                                        <p>No link provided</p>
+                                        <Skeleton className="h-6 w-20"></Skeleton>
                                     )}
                                 </div>
                             </div>
@@ -216,7 +259,10 @@ export default function ApplicationDetailsPage({ params }) {
                             Track the progress of your application
                         </CardDescription>
                     </CardHeader>
-                    <Button className="mb-6 ml-6 mr-0 w-fit sm:mb-0 sm:ml-0 sm:mr-6">
+                    <Button
+                        className="mb-6 ml-6 mr-0 w-fit sm:mb-0 sm:ml-0 sm:mr-6"
+                        disabled={isLoading}
+                    >
                         Add Event
                     </Button>
                 </div>
@@ -225,18 +271,25 @@ export default function ApplicationDetailsPage({ params }) {
                         <li className="mb-10 ms-4">
                             <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full bg-primary"></div>
                             <h3 className="font-semibold">Applied</h3>
-                            <time className="mb-2 block text-sm font-normal leading-none text-muted-foreground">
-                                {application?.date_applied &&
-                                    format(
+                            {application?.date_applied ? (
+                                <time className="mb-2 block text-sm font-normal leading-none text-muted-foreground">
+                                    {format(
                                         application.date_applied,
                                         "MMM d, yyyy",
                                     )}
-                            </time>
-                            <p className="mb-4 text-sm">
-                                {application?.position &&
-                                    application?.company &&
-                                    `Applied for ${application.position} position at ${application.company}`}
-                            </p>
+                                </time>
+                            ) : (
+                                <Skeleton className="mb-2 h-[14px] w-[78px]" />
+                            )}
+                            {application?.position && application?.company ? (
+                                <p className="mb-4 text-sm">
+                                    {application?.position &&
+                                        application?.company &&
+                                        `Applied for ${application.position} position at ${application.company}`}
+                                </p>
+                            ) : (
+                                <Skeleton className="h-5 w-60"></Skeleton>
+                            )}
                         </li>
                     </ol>
                 </CardContent>
@@ -257,8 +310,21 @@ export default function ApplicationDetailsPage({ params }) {
                         className="mb-6 resize-none"
                         value={notes}
                         onChange={(e) => setNotes(e.target.value)}
+                        disabled={isLoading}
                     />
-                    <Button className="w-fit" onClick={handleSaveNotes}>Save Notes</Button>
+                    <Button
+                        className="w-28"
+                        onClick={handleSaveNotes}
+                        disabled={
+                            isLoading || application?.notes === notes || !notes
+                        }
+                    >
+                        {isLoading ? (
+                            <Loader2 className="h-4 w-4 animate-spin text-primary-foreground/80 dark:text-muted-foreground" />
+                        ) : (
+                            "Save Notes"
+                        )}
+                    </Button>
                 </CardContent>
             </Card>
         </div>
